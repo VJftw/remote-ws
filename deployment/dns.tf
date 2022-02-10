c585c5eb886f2edff59016c35e96fedc79c01d22
VJ Patel <vj@vjpatel.me>
2022-02-10 01:09:24 +0000

    set project for google-beta provider

--- deployment/dns.tf
+++ deployment/dns.tf
@@ -1,25 +1,9 @@
-data "google_dns_managed_zone" "dns" {
-  project = "vjp-dns"
-  name    = "vjp-dns"
-}
-
 resource "google_project_service" "dns" {
   provider = google-beta
 
   service = "dns.googleapis.com"
 
   disable_dependent_services = true
-}
-
-
-resource "google_dns_record_set" "remote-ws" {
-  name = "remote-ws.${data.google_dns_managed_zone.dns.dns_name}"
-  type = "A"
-  ttl  = 60
-
-  managed_zone = data.google_dns_managed_zone.dns.name
-
-  rrdatas = [google_compute_instance.workspace.network_interface[0].access_config[0].nat_ip]
 }
 
 resource "google_service_account" "remote-ws" {
--- deployment/main.tf
+++ deployment/main.tf
@@ -2,3 +2,8 @@ provider "google" {
   project = var.name
   region  = "europe-west2"
 }
+
+provider "google-beta" {
+  project = var.name
+  region  = "europe-west2"
+}
