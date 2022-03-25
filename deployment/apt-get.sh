b2272f92d4e60fa9adbc684e0837e5efe59c0646
VJ Patel <VJftw@users.noreply.github.com>
2022-03-25 20:35:20 +0000

    Update apt-get.sh

--- deployment/apt-get.sh
+++ deployment/apt-get.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 # This script wraps apt-get allowing it to be used concurrently.
-# From: https://askubuntu.com/a/375031
+# From: https://askubuntu.com/a/373478
 set -Eeuo pipefail
 
-i=0
-tput sc
-while fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
-    case $(($i % 4)) in
-        0 ) j="-" ;;
-        1 ) j="\\" ;;
-        2 ) j="|" ;;
-        3 ) j="/" ;;
-    esac
-    tput rc
-    echo -en "\r[$j] Waiting for other software managers to finish..." 
-    sleep 0.5
-    ((i=i+1))
-done 
+while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock \
+    >/dev/null 2>&1; do
+   sleep 0.5
+done
 
 /usr/bin/apt-get "$@"
