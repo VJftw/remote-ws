2fd362f2d97565348cd0fe7afef1ec1e9293be1c
VJ Patel <VJftw@users.noreply.github.com>
2022-10-24 22:09:35 +0100

    Update main.tf

--- deployment/main.tf
+++ deployment/main.tf
@@ -14,7 +14,7 @@ module "vjftw" {
   ssh_public_keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAAd2c9cCa12ozaNjeEjTShxj92oMyiwiiIQ+5buXj2D vj@SIRIUS",
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP9RT6lp86XpwrC8R7jFfnm3DV+PAVNP2Rbt7YjX6tJE vj@DUMBLEDORE",
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5q5E76PTrArH8W9fJej7Pz7FqlpltvdepTt45k5Zha vj@vjpatel.me"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM4Y2QHqM1KAmXAyxtS61gSMeum27slKZCGaSXS6/LRb vj@vjpatel.me"
   ]
 
   machine_type = "n2d-standard-8"
